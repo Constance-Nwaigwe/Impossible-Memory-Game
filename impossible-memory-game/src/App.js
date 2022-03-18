f52f57c6092b3e29446b9deab01e58069c66501b
Constance Nwaigwe <constance.nwaigwe@aura.com>
2022-03-18 13:14:21 -0400

    added functionality and styling

--- impossible-memory-game/src/App.js
+++ impossible-memory-game/src/App.js
@@ -1,15 +1,89 @@
+import React, { useState } from "react";
 import "./App.css";
 import Gamedata from "./components/data";
+import "./images/carrot.png";
 
 function App() {
+  let turn = 1;
+  let guess1 = null;
+  let guess2 = null;
+  let gameActive = false;
+  let clickCounter = 0;
+  const [gameStatus, SetGameStatus] = useState("Ready To Win");
+  const [isclicked, Setisclicked] = useState("flipcard");
+
+  function startGame() {
+    gameActive = true;
+    Gamedata.forEach((d) => {
+      d.clicked = false;
+    });
+    clickCounter = 0;
+  }
+
+  function runMatch(guess) {
+    if (gameActive === true && guess.clicked === false) {
+      if (turn === 1) {
+        guess1 = guess.number;
+        console.log("turn", turn);
+        turn = 2;
+      } else {
+        guess2 = guess.number;
+        console.log("turn", turn);
+        turn = 1;
+        if (guess1 !== guess2) {
+          gameActive = false;
+          SetGameStatus("You have lost");
+          console.log("You have lost");
+        }
+      }
+      clickCounter++;
+      guess.clicked = true;
+      if (guess.clicked === true) {
+        Setisclicked("flipcard");
+        console.log(guess.clicked, isclicked);
+      }
+
+      if (clickCounter === 10) {
+        console.log("You have won");
+        SetGameStatus("You have won");
+      }
+      console.log(clickCounter);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to Impossible Memory Card Game.</h1>
+        <p>Welcome to Impossible Memory Card Game.</p>
       </header>
-      <section>
+      <button className="start" onClick={() => startGame()}>
+        Start Game
+      </button>
+      <div>
+        <p>Game Status: {gameStatus}</p>
+      </div>
+      <section className="gameboard">
         {Gamedata.map((d) => (
-          <p>{d.image}</p>
+          <div
+            onClick={() => {
+              runMatch(d);
+            }}
+            className={`flip-card ${isclicked}`}
+          >
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <h1>Impossible Memory Card Game</h1>
+                <img
+                  className="frontimg"
+                  src="https://ih1.redbubble.net/image.310166659.4641/poster,504x498,f8f8f8-pad,600x600,f8f8f8.u2.jpg"
+                  alt="froggie"
+                />
+              </div>
+              <div className="flip-card-back">
+                <img className="cardimg" src={d.image} alt={d.image} />
+              </div>
+            </div>
+          </div>
         ))}
       </section>
     </div>
@@ -17,3 +91,12 @@ function App() {
 }
 
 export default App;
+
+/*<div
+            className="gameimage"
+            onClick={() => {
+              runMatch(d.id);
+            }}
+          >
+            <img src={d.image} alt={d.image} />
+          </div> */
--- impossible-memory-game/src/components/data.js
+++ impossible-memory-game/src/components/data.js
@@ -1,53 +1,73 @@
 const Gamedata = [
   {
     id: 1,
-    image: "Image url",
+    image:
+      "https://www.clipartmax.com/png/small/28-289845_cenoura-cartoon-fruit-and-vegetables.png",
     number: 1,
+    clicked: true,
   },
   {
-    id: 1,
-    image: "Image url",
-    number: 1,
+    id: 2,
+    image:
+      "https://flyclipart.com/thumb2/fruit-vegetable-cartoon-clip-art-and-art-797824.png",
+    number: 2,
+    clicked: false,
   },
   {
-    id: 1,
-    image: "Image url",
-    number: 1,
+    id: 3,
+    image:
+      "https://www.seekpng.com/png/detail/550-5500624_clip-freeuse-clipart-fruits-and-veggies-cartoon-vegetables.png",
+    number: 3,
+    clicked: false,
   },
   {
-    id: 1,
-    image: "Image url",
-    number: 1,
+    id: 4,
+    image:
+      "https://www.kindpng.com/picc/m/46-467635_calabaza-animation-fruits-vegetables-cartoon-vegetables-and-fruits.png",
+    number: 4,
+    clicked: false,
   },
   {
-    id: 1,
-    image: "Image url",
-    number: 1,
+    id: 5,
+    image:
+      "https://www.vhv.rs/dpng/d/175-1752568_clip-art-funny-vegetables-cartoon-fruits-and-vegetables.png",
+    number: 5,
+    clicked: false,
   },
   {
-    id: 1,
-    image: "Image url",
+    id: 6,
+    image:
+      "https://www.clipartmax.com/png/small/28-289845_cenoura-cartoon-fruit-and-vegetables.png",
     number: 1,
+    clicked: false,
   },
   {
-    id: 1,
-    image: "Image url",
-    number: 1,
+    id: 7,
+    image:
+      "https://flyclipart.com/thumb2/fruit-vegetable-cartoon-clip-art-and-art-797824.png",
+    number: 2,
+    clicked: false,
   },
   {
-    id: 1,
-    image: "Image url",
-    number: 1,
+    id: 8,
+    image:
+      "https://www.seekpng.com/png/detail/550-5500624_clip-freeuse-clipart-fruits-and-veggies-cartoon-vegetables.png",
+    number: 3,
+    clicked: false,
   },
   {
-    id: 1,
-    image: "Image url",
-    number: 1,
+    id: 9,
+    image:
+      "https://www.kindpng.com/picc/m/46-467635_calabaza-animation-fruits-vegetables-cartoon-vegetables-and-fruits.png",
+    number: 4,
+    clicked: false,
   },
   {
-    id: 1,
-    image: "Image url",
-    number: 1,
+    id: 10,
+    image:
+      "https://www.vhv.rs/dpng/d/175-1752568_clip-art-funny-vegetables-cartoon-fruits-and-vegetables.png",
+    number: 5,
+    clicked: false,
   },
 ];
 
